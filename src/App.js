5e9a296803bb8b4824f92d440037346c646047fd
heejin <dlwlrgkwk82@gmail.com>
2023-09-24 18:04:32 +0900

    feat: add body, root css

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const filters = ["all", "active", "completed"];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
+    <>
       <HeaderFilter filters={filters} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </div>
+    </>
   );
 }
 
